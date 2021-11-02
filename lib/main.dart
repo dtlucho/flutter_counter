052e934254a1a515299b97af38298abb2b55f68f
dtlucho <luciano.ditomaso@gmail.com>
2021-11-02 16:09:57 -0300

    Folder structure. Dividing functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:counter/src/app.dart';
+
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text('Hola mundo'),
-      ),
-    );
-  }
 }
