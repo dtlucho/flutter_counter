0afe7b444a0807f34aea21e54f42708fa763cded
dtlucho <luciano.ditomaso@gmail.com>
2022-01-20 22:34:34 -0300

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'package:counter/src/app.dart';
+import 'package:flutter_counter/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen(),
+    );
+  }
 }
